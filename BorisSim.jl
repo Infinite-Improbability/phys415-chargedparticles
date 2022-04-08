f35286d45b8bb21ea344408c34b0915a0ffa5611
Infinite-Improbability <coxryan@myvuw.ac.nz>
2022-04-08 18:25:24 +1200

    Report 1

--- BorisSim.jl
+++ BorisSim.jl
@@ -1,6 +1,7 @@
 using LinearAlgebra: cross, dot, norm
 using Plots
 using Unitful
+using UnitfulRecipes
 
 mutable struct Particle
     """Properties of a single particle in the system."""
@@ -11,11 +12,14 @@ mutable struct Particle
 end
 
 # Define electric field as function of position
-E(r::Vector)::Vector = [0u"V/m", 0u"V/m", 0.01u"V/m"]
+# E(r::Vector)::Vector = [0u"V/m", 0u"V/m", 0.01u"V/m"]
+cutoff = -4e-7u"m"
+E(r::Vector)::Vector = r[3] > cutoff ? [0u"V/m", 0u"V/m", 0.01u"V/m"] : [0u"V/m", 0u"V/m", 0.00u"V/m"]
 # We aren't actually using the position here but by passing it we make
 # it easier in the future
 
 # Define magnetic field as function of position
+# B(r::Vector)::Vector = [0u"T", 0u"T", 0.01u"T"]
 B(r::Vector)::Vector = [0u"T", 0u"T", 0.01u"T"]
 
 function stepVelocity!(part::Particle, dt::Quantity)
@@ -39,33 +43,60 @@ end
 function lamorRadius(part::Particle)::Quantity
     """Calculate lamorRadius of particle."""
     # First want the tangential velocity
-    n = part.r / norm(part.r) # unit vector of position, normal to surface
+    n = B(p1.r) / norm(B(p1.r)) # unit vector of magnetic field
     vTang = norm(part.v - dot(part.v, n) * n) # v⟂ = v - v∥ = v - (v⋅n)n
-
     return part.m * vTang / part.q / norm(B(part.r))
 end
 
 # Init system
 dt = 3e-11u"s" # timestep
 iterations = 1000
-p1 = Particle([0u"m", 0u"m", 0u"m"], [0u"m/s", 1e5u"m/s", 0u"m/s"], -1.602e-19u"C", 9.109e-31u"kg")
-stepVelocity!(p1, -dt / 2) # Offset initial velocity back 1/2 step
+# p1 = Particle([0u"m", 0u"m", 0u"m"], [0u"m/s", 1e5u"m/s", 0u"m/s"], -1.602e-19u"C", 9.109e-31u"kg")
+p1 = Particle([1.0u"m", 0u"m", 0u"m"], [0.0u"m/s", 1u"m/s", 0u"m/s"], -1.602e-19u"C", 9.109e-31u"kg")
+p1.r[1] = -lamorRadius(p1) # adjust particle position so that it rotates around origin. Polarity dependent
+display(p1)
+
+# Analytical solutions
+time = 0u"s" : dt : (dt*(iterations-1))
+
+# E=0, B=const, assumes orbit around origin, B up in z only
+ω = p1.q * norm(B(p1.r)) / p1.m # gyrofrequency
+pos = zeros(typeof(1.0u"m"), (1000,3))
+for i in 1:iterations
+    θ = -ω*time[i]
+    pos[i,:] = [cos(θ) -sin(θ) 0; sin(θ) cos(θ) 0; 0 0 1] * p1.r # rotate about z
+end
+
+# E=[0,0,0.01] when z>cutoff, B=0
+accel = p1.q * E([0.0u"m", 0u"m", 0u"m"]) / p1.m
+tField = sqrt(abs(2*cutoff/accel[3])) # time to clear E field
+vField = accel*tField # velocity gained in this time
+Δd(t) = t <= tField ? (t*p1.v+0.5*accel*t^2)[3] : cutoff + vField[3]*(t-tField)
+pos1 = (time*p1.v')[:,3] + Δd.(time) # row i is [x y z] at time[i]
+
+# Combine E and B contributions
+pos[:,3] += pos1
 
 # Init plot
 # We'll base the limits on the Larmor radius of the particle
-lim = 1.1 * lamorRadius(p1)
+# lim = ustrip(1.1 * lamorRadius(p1)) # Fails if initial pos is origin, code executes with auto limits
 plt = plot3d(
     1, # a single empty series
-    xlim=(-lim, lim),
-    ylim=(-lim, lim),
-    zlim=(-lim, lim),
-    title="Charged particle goes zoom",
+    # lims=(-lim, lim),
+    # title="E=const B=0",
     marker=2,
     legend=:none,
-    xlabel="x (m)",
-    ylabel="y (m)",
-    zlabel="z (m)"
+    xlabel="x",
+    ylabel="y",
+    zlabel="z",
+    #line_z=ustrip.(time), # colour line based on time
+    #colorbar=:bottom,
+    label="Numerical",
+    #markershape=:none
 )
+
+# Offset initial velocity back 1/2 step so it leapfrogs with position
+stepVelocity!(p1, -dt / 2) 
 
 # Main loop
 anim = @gif for i in 1:iterations
@@ -78,8 +109,20 @@ anim = @gif for i in 1:iterations
     # This at least works.
 end every 10 # save every tenth run as a frame
 
-print("GIF saved at $(anim.filename)")
-display(anim)
 # The gif macro doesn't allow a custom output location.
 # It would be possible if I went more hands on in
 # the gif construction but it doesn't seem worth the effort.
+# You cqn export the last frame as an image with
+# savefig("filename")
+print("GIF saved at $(anim.filename)\n")
+display(anim)
+
+#Add analytical solution to plot
+plot!(pos[:,1], pos[:,2], pos[:,3],
+    label="Analytical",
+    legend=:right,
+    # line_z=ustrip.(time),
+    lims=:auto)
+display(plt)
+
+print("Magnitude of error in final positions: $(norm(pos[end,:]-p1.r))\n")

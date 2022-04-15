db672b35dac4b4e7914f5502442607dbd7bdbb9d
Infinite-Improbability <coxryan@myvuw.ac.nz>
2022-04-15 21:01:12 +1200

    Improve comments

--- BorisSim.jl
+++ BorisSim.jl
@@ -11,12 +11,15 @@ mutable struct Particle
     m::Quantity # mass
 end
 
-# Define electric field as function of position
+# Define the electric and magnetic fields.
+# They are always defined as functions of position - even for
+# constant fields - so that you don't have to go through changing
+# the Boris method code whenever you switch to a position dependent field.
+# The field functions can be anything that returns a three element vector with units
+# appropriate to the field type.
+# Units are denoted using Unitful's notation.
+# e.g. 1u"V/m" is 1 Volt per metre
 E(r::Vector)::Vector = [0u"V/m", 0u"V/m", 0.01u"V/m"]
-# We aren't actually using the position here but by passing it we make
-# it easier in the future
-
-# Define magnetic field as function of position
 B(r::Vector)::Vector = [0u"T", 0u"T", 0.01u"T"]
 
 function stepVelocity!(part::Particle, dt::Quantity)
@@ -32,10 +35,12 @@ function stepVelocity!(part::Particle, dt::Quantity)
     part.v = vPlus + halfEV
 end
 
+
 function stepPosition!(part::Particle, dt::Quantity)
     """Update position over timestep dt with current velocity."""
     part.r += part.v * dt
 end
+
 
 function lamorRadius(part::Particle)::Quantity
     """Calculate lamorRadius of particle."""
@@ -45,39 +50,50 @@ function lamorRadius(part::Particle)::Quantity
     return part.m * vTang / part.q / norm(B(part.r))
 end
 
+
 # Init system
+print("Initialising system and plot.\n")
 dt = 3e-11u"s" # timestep
 iterations = 1000
-p1 = Particle([0u"m", 0u"m", 0u"m"], [0u"m/s", 1e5u"m/s", 0u"m/s"], -1.602e-19u"C", 9.109e-31u"kg")
+
+# Define particle, in inital state.
+p1 = Particle(
+    [0u"m", 0u"m", 0u"m"], # Initial position
+    [0u"m/s", 1e5u"m/s", 0u"m/s"], # Inital velocity
+    -1.602e-19u"C", # Charge
+    9.109e-31u"kg" # Mass
+)
 
 # Init plot
+# Plot attributes have been chosen to cleanly represent
+# a single particle with no analytical line.
+# For more options see https://docs.juliaplots.org/stable/attributes/
 plt = plot3d(
     1, # a single empty series
     # title="Some title",
-    marker=2,
     legend=:none,
-    xlabel="x",
-    ylabel="y",
-    zlabel="z",
-    line_z=ustrip.(time), # colour line based on time
-    colorbar=:none, # :bottom, :left etc
+    xlabel="x (m)",
+    ylabel="y (m)",
+    zlabel="z (m)",
     label="Numerical",
     markershape=:none
 )
 
 # Offset initial velocity back 1/2 step so it leapfrogs with position
-stepVelocity!(p1, -dt / 2) 
+stepVelocity!(p1, -dt / 2)
 
 # Main loop
+print("Beginning loop.\n")
 anim = @gif for i in 1:iterations
     # ! indicates inplace functions
     stepVelocity!(p1, dt)
     stepPosition!(p1, dt)
-    push!(plt, ustrip.(u"m", p1.r)...) # expands out positions
+    push!(plt, ustrip.(u"m", p1.r)...) # ... expands out positions. ustrip applied elementwise due to . before brackets
     # Pushing Quantities to the plot failed even when using UnitfulRecipes
     # (a package to give Plots unit support)
     # This at least works.
 end every 10 # save every tenth run as a frame
+print("Loop complete.\n")
 
 # The gif macro doesn't allow a custom output location.
 # It would be possible if I went more hands on in
@@ -85,4 +101,12 @@ end every 10 # save every tenth run as a frame
 # You cqn export the last frame as an image with
 # savefig("filename")
 print("GIF saved at $(anim.filename)\n")
-display(anim)
+# This is probably unneeded, as Julia Plots prints this anyway,
+# But I include it just in case.
+
+# The GUI display can only handle the last frame, not the animation.
+# It also doesn't seem to work when running in the REPL from VS Code,
+# but is helpful if funning in the terminal.
+gui()
+print("Press any key to exit ") # Don't immediately close on us
+readline()
--- README.md
+++ README.md
@@ -7,8 +7,11 @@ Currently supports a single particle modelled using the Boris method.
 To run this code you will need Julia. The [latest stable release](https://julialang.org/downloads/#current_stable_release) (1.7.2 at time of writing) is recommended. You will also need some additional packages, listed in `Project.toml`. The easiest way to install them is to use Julia's [inbuilt package manager](https://pkgdocs.julialang.org/v1/), as follows:
 1. Clone the repo and `cd` inside
 2. Start a Julia REPL session with `julia`
-3. Switch to package management mode by typing `]`
+3. Switch to package management mode by typing `]`.
+
 4. Activate the project with `activate .`
 5. Run `instantiate` to install the dependencies defined in the project manifest.
+ 
+To run a Julia script inside this environment use `julia --project=. BorisSim.jl` from your terminal. If you prefer to launch from the REPL, just run `include("BorisSim.jl")` with the project environment active, as described above. You'll need to exit package management first, by pressing backspace on an empty line in the REPL.
 
-To run a Julia script inside this environment use `julia --project=. scriptName.jl`.
+To define the parameters of the scenario, see the code comments.
